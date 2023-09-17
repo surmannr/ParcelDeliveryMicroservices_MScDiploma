5a88f938d79a2e954653afb1f49f0337603a1fc2
Surmann Roland <surmannroland@gmail.com>
2023-09-17 20:46:54 +0200

    Ütemező elkészítése

--- backend/src/Microservices/PackageDelivery/PackageDelivery.API/Program.cs
+++ backend/src/Microservices/PackageDelivery/PackageDelivery.API/Program.cs
@@ -1,13 +1,17 @@
 using Autofac;
+using Autofac.Core;
 using Autofac.Extensions.DependencyInjection;
 using Common.Entity;
 using Common.Extension.CQRS;
 using Common.Serializers;
 using EventBus.Messages.Common;
 using FluentValidation;
+using Hangfire;
+using Hangfire.SqlServer;
 using MassTransit;
 using MediatR;
-using MongoDB.Bson.Serialization;
+using System.Data.SqlClient;
+using PackageDelivery.API;
 using PackageDelivery.API.EventBusConsumer;
 using PackageDelivery.BL.Algorithms;
 using PackageDelivery.BL.Extensions.Identity;
@@ -17,6 +21,8 @@ using PackageDelivery.BL.Services;
 using PackageDelivery.DAL;
 using PackageDelivery.DAL.Repositories;
 using System.Reflection;
+using MongoDB.Driver.Core.Configuration;
+using Microsoft.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,6 +35,40 @@ builder.Services.AddCors(options =>
             .AllowAnyOrigin()
             .AllowAnyHeader());
 });
+#endregion
+
+using (var conn = new SqlConnection(builder.Configuration.GetConnectionString("HangfireConnectionCreateDb")))
+{
+    conn.Open();
+    string commandtext = "IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'ParcelDelivery_SchedulerDb')\r\nBEGIN\r\n  CREATE DATABASE ParcelDelivery_SchedulerDb;\r\nEND;\r\n";
+
+    var cmd = new SqlCommand(commandtext, conn);
+
+    var reader = cmd.ExecuteReader();
+}
+
+#region Hangfire
+builder.Services.AddHangfire(configuration => configuration
+       .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+       .UseSimpleAssemblyNameTypeSerializer()
+       .UseRecommendedSerializerSettings()
+       .UseSqlServerStorage(builder.Configuration.GetConnectionString("HangfireConnection"), new SqlServerStorageOptions
+       {
+           CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+           SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+           QueuePollInterval = TimeSpan.Zero,
+           UseRecommendedIsolationLevel = true,
+           DisableGlobalLocks = true
+       }));
+
+builder.Services.AddHangfireServer(options =>
+{
+    options.Queues = new[] {
+                    "algorithm",
+                    "default" };
+});
+
+builder.Services.AddHangfireServer();
 #endregion
 
 #region Automapper
@@ -109,6 +149,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors("CorsPolicy");
+
+app.UseHangfireDashboard("/hangfire", new DashboardOptions
+{
+    Authorization = new[] { new HangfireAuthorizationFilter() }
+});
+RecurringJob.AddOrUpdate<ISchedulingService>((schedulingService) => schedulingService.Schedule(), Cron.Daily, queue: "algorithm");
 
 app.UseAuthorization();
 
