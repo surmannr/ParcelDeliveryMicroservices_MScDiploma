6d74cdf41d9b919ebcb5e790b6a11e23fab7db63
Surmann Roland <surmannroland@gmail.com>
2023-10-23 14:45:38 +0200

    Üdvözlőképernyők

--- frontend/mobil/parceldelivery_mobile/lib/screens/welcome/customer_welcome.dart
+++ frontend/mobil/parceldelivery_mobile/lib/screens/welcome/customer_welcome.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:parceldelivery_mobile/constants.dart';
 import 'package:parceldelivery_mobile/frame.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class WelcomeCustomer extends StatefulWidget {
   const WelcomeCustomer({super.key});
@@ -13,12 +15,39 @@ class WelcomeCustomer extends StatefulWidget {
 class _WelcomeCustomerState extends State<WelcomeCustomer> {
   @override
   Widget build(BuildContext context) {
-    return const FrameScaffold(
-      child: Column(
-        children: [
-          Text("Welcome"),
-        ],
-      ),
+    return FrameScaffold(
+      child: FutureBuilder(
+          future: SharedPreferences.getInstance(),
+          builder: (ctx, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              // If we got an error
+              if (snapshot.hasError) {
+                return const Center(
+                  child: Text(
+                    'Hiba történt',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                );
+              } else if (snapshot.hasData) {
+                return Center(
+                  child: Text(
+                    "Üdvözöljük ${snapshot.data?.getString(Constants.sharedPref.nameTag) ?? ""}",
+                    style: const TextStyle(
+                      fontSize: 22,
+                    ),
+                  ),
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
     );
   }
 }
